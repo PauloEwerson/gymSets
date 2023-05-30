f18e4d9193aa6a802e0e0d569de678ee27388395
PauloEwerson <ewersonmrt@gmail.com>
2023-05-30 13:46:30 -0300

    feat: Adiciona a logo da aplicação na interface de SignIn

--- src/screens/SigIn.tsx
+++ src/screens/SigIn.tsx
@@ -1,16 +1,25 @@
-import { VStack, Image } from 'native-base';
+import { VStack, Image, Text, Center } from 'native-base';
 
+import LogoSvg from '@assets/logo.svg';
 import Background from '@assets/background.png';
 
 export function SigIn() {
   return (
     <VStack flex={1} bg="gray.700">
-      <Image 
+      <Image
         source={Background}
         alt="Background com pessoas treinando"
         resizeMode="contain"
         position="absolute"
       />
+
+      <Center my={24}>
+        <LogoSvg />
+
+        <Text color="gray.100" fontSize="sm">
+          Treine sua mente e o seu corpo
+        </Text>
+      </Center>
     </VStack>
   )
 }
