35fa6f9c1884ffc06fc9831fa589b8ede860a956
PauloEwerson <ewersonmrt@gmail.com>
2023-06-06 21:22:38 -0300

    feat: Adiciona uma ScrollView do NativeBase por volta de todos os componentes para melhorarmos a experiência do usuário quando o teclado estiver aberto.

--- src/screens/SigIn.tsx
+++ src/screens/SigIn.tsx
@@ -1,4 +1,11 @@
-import { VStack, Image, Text, Center, Heading } from 'native-base';
+import {
+  VStack,
+  Image, 
+  Text,
+  Center,
+  Heading,
+  ScrollView
+} from 'native-base';
 
 import LogoSvg from '@assets/logo.svg';
 import Background from '@assets/background.png';
@@ -8,46 +15,65 @@ import { Button } from '@components/Button';
 
 export function SigIn() {
   return (
-    <VStack flex={1} bg="gray.700" px={10}>
-      <Image
-        source={Background}
-        alt="Background com pessoas treinando"
-        resizeMode="contain"
-        position="absolute"
-      />
-
-      <Center my={24}>
-        <LogoSvg />
-
-        <Text color="gray.100" fontSize="sm">
-          Treine sua mente e o seu corpo
-        </Text>
-      </Center>
-
-      <Center>
-        <Heading
-          color="gray.100"
-          fontSize="xl"
-          mb={6}
-          fontFamily="heading"
-        >
-          Acesse sua conta
-        </Heading>
-
-        <Input
-          placeholder='E-mail'
-          keyboardType='email-address'
-          autoCapitalize='none' // não capitaliza o texto
-        />
-        <Input
-          placeholder='Senha'
-          secureTextEntry // não mostra o texto digitado  
+    <ScrollView
+      contentContainerStyle={{ flexGrow: 1 }} // corrige o scroll
+      showsHorizontalScrollIndicator={false} // remove a barra de scroll
+    >
+      <VStack
+        flex={1}
+        bg="gray.700"
+        px={10}
+        pb={16} // Adicionado para corrigir o scroll
+      >
+        <Image
+          source={Background}
+          alt="Background com pessoas treinando"
+          resizeMode="contain"
+          position="absolute"
         />
 
-        <Button title="Acessar" />
+        <Center my={24}>
+          <LogoSvg />
 
-        <Button title="Criar conta" variant="outline"/>
-      </Center>
-    </VStack>
+          <Text color="gray.100" fontSize="sm">
+            Treine sua mente e o seu corpo
+          </Text>
+        </Center>
+
+        <Center>
+          <Heading
+            color="gray.100"
+            fontSize="xl"
+            mb={6}
+            fontFamily="heading"
+          >
+            Acesse sua conta
+          </Heading>
+
+          <Input
+            placeholder='E-mail'
+            keyboardType='email-address'
+            autoCapitalize='none' // não capitaliza o texto
+          />
+          <Input
+            placeholder='Senha'
+            secureTextEntry // não mostra o texto digitado  
+          />
+
+          <Button title="Acessar" />
+        </Center>
+
+        <Center mt={24}>
+          <Text color="gray.100" fontSize="sm" mb={3} fontFamily="body">
+            Ainda não tenho acesso
+          </Text>
+
+          <Button
+            title="Criar conta"
+            variant="outline"
+          />
+        </Center>
+      </VStack>
+    </ScrollView>
   )
 }
