0b848ba8fe6e78a9ed902ec9c4fd6c309247916f
PauloEwerson <ewersonmrt@gmail.com>
2023-06-08 19:52:11 -0300

    feat: Adiciona uma propriedade isActive para o componente Group e utilizar o estado da aplicação para indicar o estado selecionado.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { HStack, VStack } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
@@ -5,13 +6,22 @@ import { Group } from '@components/Group';
 
 
 export function Home() {
+  const [groupSelected, setGroupSelected] = useState('costa')
   return (
     <VStack flex={1}>
       <HomeHeader />
 
       <HStack>
-        <Group name="Costas" />
-        <Group name="Ombro" />
+        <Group
+          name="costa"
+          isActive={groupSelected === 'costa'}
+          onPress={() => setGroupSelected('costa')}
+        />
+        <Group
+          name="ombro"
+          isActive={groupSelected === 'ombro'}
+          onPress={() => setGroupSelected('ombro')}
+        />
       </HStack>
     </VStack>
   );
