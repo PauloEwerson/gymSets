8c3b253bb9801d34e514c58a6b756e11ef3b7d99
PauloEwerson <ewersonmrt@gmail.com>
2023-06-08 21:03:31 -0300

    feat: Cria o componente ExerciseCard, responsável por exibir os detalhes do exercício no momento da listagem, e limita a quantidade de linhas que serão exibidas utilizando o numberOfLines.

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -3,6 +3,7 @@ import { VStack, FlatList, HStack, Heading, Text } from 'native-base';
 
 import { HomeHeader } from '@components/HomeHeader';
 import { Group } from '@components/Group';
+import { ExerciseCard } from '@components/ExerciseCard';
 
 
 export function Home() {
@@ -40,6 +41,9 @@ export function Home() {
             4
           </Text>
         </HStack>
+
+        <ExerciseCard />
+        <ExerciseCard />
       </VStack>
     </VStack>
   );
