e36c97d09de9981228b903b19572fd6d921b1f6a
PauloEwerson <ewersonmrt@gmail.com>
2023-06-08 22:54:11 -0300

    feat: Utiliza a SectionList para formatar e exibir dados na aplicação.

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,16 +1,39 @@
-import { VStack } from 'native-base';
+import { useState } from 'react';
+import { Heading, VStack, SectionList } from 'native-base';
 
 import { ScreenHeder } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
 
 export function History() {
+
+  const [exercises, setExercises] = useState([
+    {
+      title: '08.06.23',
+      data: ['Agachamento', 'Supino', 'Rosca Direta']
+    },
+    {
+      title: '09.06.23',
+      data: ['Puxada frontal']
+    },
+  ]);
+
   return (
     <VStack flex={1}>
       <ScreenHeder title="Histórico de Exercícios" />
 
-      <HistoryCard />
-      <HistoryCard />
-      <HistoryCard />
+      <SectionList
+        sections={exercises}
+        keyExtractor={(item, index) => item + index}
+        renderItem={({ item }) => (
+          <HistoryCard />
+        )}
+        renderSectionHeader={({section}) => (
+          <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
+            {section.title}
+          </Heading>
+        )}
+        px={8}
+      />
     </VStack>
   );
 }
