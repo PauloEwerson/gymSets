3e463e53274d5781ced6902927a5fcb378fcf87f
PauloEwerson <ewersonmrt@gmail.com>
2023-06-09 15:58:55 -0300

    feat:  Adiciona ListEmptyComponent e quebra de linha com {'\n'}

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Heading, VStack, SectionList } from 'native-base';
+import { Heading, VStack, SectionList, Text } from 'native-base';
 
 import { ScreenHeder } from '@components/ScreenHeader';
 import { HistoryCard } from '@components/HistoryCard';
@@ -27,12 +27,19 @@ export function History() {
         renderItem={({ item }) => (
           <HistoryCard />
         )}
-        renderSectionHeader={({section}) => (
+        renderSectionHeader={({ section }) => (
           <Heading color="gray.200" fontSize="md" mt={10} mb={3}>
             {section.title}
           </Heading>
         )}
         px={8}
+        contentContainerStyle={[].length === 0 && { flex: 1, justifyContent: 'center' }}
+        ListEmptyComponent={() => (
+          <Text color="gray.100" textAlign="center">
+            Não há exercícios registrados ainda. {'\n'}
+            Vamos fazer exercicíos hoje?
+          </Text>
+        )}
       />
     </VStack>
   );
