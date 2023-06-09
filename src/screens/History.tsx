c14a5399694ca0607714bb0e9e3a4015a783dadb
PauloEwerson <ewersonmrt@gmail.com>
2023-06-08 22:37:48 -0300

    feat: Cria componente HistoryCard para exibir detalhes do histórico de exercícios.

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,13 +1,16 @@
 import { VStack } from 'native-base';
 
 import { ScreenHeder } from '@components/ScreenHeader';
+import { HistoryCard } from '@components/HistoryCard';
 
 export function History() {
   return (
     <VStack flex={1}>
-      <ScreenHeder
-        title="Histórico de Exercícios"
-      />
+      <ScreenHeder title="Histórico de Exercícios" />
+
+      <HistoryCard />
+      <HistoryCard />
+      <HistoryCard />
     </VStack>
   );
 }
