1bc0750740205b5ef1d7307b6e48295678f771f4
PauloEwerson <ewersonmrt@gmail.com>
2023-06-09 18:40:22 -0300

    feat: Ajusta o título da seção e padding da ScrollView

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -14,7 +14,8 @@ export function Profile() {
   return (
     <VStack flex={1}>
       <ScreenHeader title="Perfil" />
-      <ScrollView>
+
+      <ScrollView contentContainerStyle={{paddingBottom: 36 }}>
         <Center mt={6} px={10}>
           {photoIsLoading
             ? (
@@ -55,10 +56,14 @@ export function Profile() {
             placeholder="E-mail"
             isDisabled
           />
-        </Center>
 
-        <VStack px={10} mt={12} mb={9}>
-          <Heading color="gray.200" fontSize="md" mb={2}>
+          <Heading
+            color="gray.200"
+            fontSize="md"
+            mb={2}
+            mt={12}
+            alignSelf="flex-start"
+          >
             Alterar senha
           </Heading>
 
@@ -85,7 +90,7 @@ export function Profile() {
             mt={4}
           />
 
-        </VStack>
+        </Center>
       </ScrollView>
     </VStack>
   );
