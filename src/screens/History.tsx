faa08163dfd2b9828f123f2e553cfa6cb8f385fb
PauloEwerson <ewersonmrt@gmail.com>
2023-06-08 22:07:04 -0300

    feat: Cria componente ScreenHeader para reutilização

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,9 +1,13 @@
-import { Center, Text } from 'native-base';
+import { VStack } from 'native-base';
+
+import { ScreenHeder } from '@components/ScreenHeader';
 
 export function History() {
   return (
-    <Center flex={1}>
-      <Text color="white">History</Text>
-    </Center>
+    <VStack flex={1}>
+      <ScreenHeder
+        title="Histórico de Exercícios"
+      />
+    </VStack>
   );
 }
